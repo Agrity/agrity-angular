d1060d2633860ec44efe65ce81ffbc12bd216feb
matthewdavies27 <matthewdavies27@gmail.com>
2016-06-29 13:22:27 -0700

    ViewUserComponent with Bids

--- app/users/view-user.component.ts
+++ app/users/view-user.component.ts
@@ -4,22 +4,26 @@ import {RouterLink, ROUTER_DIRECTIVES, RouteConfig} from '@angular/router-deprec
 import {Observable} from 'rxjs/Observable';
 import {UserService} from './user.service';
 import {User} from './user';
+import {BidService} from '../bids/bid.service';
+import {Bid} from '../bids/bid';
 import {ErrorHandling} from '../ErrorHandling';
 
 @Component({
   templateUrl: 'app/users/view-user.component.html',
   styles: [], 
   styleUrls: [],
-  providers: [UserService],
+  providers: [UserService, BidService],
   directives: [RouterLink, ROUTER_DIRECTIVES]
 })
 
 export class ViewUserComponent implements OnInit {
 
   private user: User = new User();
+  private bids: Bid[];  
 
   constructor(
     private _userService: UserService,
+    private _bidService: BidService,
     private _errorHandling: ErrorHandling) {
   }
 
@@ -32,5 +36,14 @@ export class ViewUserComponent implements OnInit {
         },
         error => this._errorHandling.handleHttpError(error));
 
+    this.bids = [];
+    this._bidService.getGrowerBids(1)
+      .subscribe(
+        bids => {
+          for (var bidIdx in bids) {
+            this.bids.push(Bid.decode(bids[bidIdx]));
+          }
+        },
+        error => this._errorHandling.handleHttpError(error));
   } 
 }
