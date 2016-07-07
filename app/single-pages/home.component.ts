fb76100a35a9d2fa78d203e1ced0ee64077e5e0f
matthewdavies27 <matthewdavies27@gmail.com>
2016-07-07 10:37:08 -0700

    Directive added back. Routing needs to be moved out of html.

--- app/single-pages/home.component.ts
+++ app/single-pages/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, ROUTER_DIRECTIVES, RouteParams, Router }
+    from '@angular/router-deprecated';
 
 @Component({
+  directives: [ROUTER_DIRECTIVES],
   templateUrl: 'app/single-pages/home.component.html',
   styleUrls: ['app/single-pages/home.component.scss'],
+
 })
 
 export class HomeComponent  {
