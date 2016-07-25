9d7802c97e3a6d0640aec8a7748dba0f185db05c
Ryan Davies <ryscot@gmail.com>
2016-07-25 14:42:45 -0700

    Seems Fixed

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,17 +5,14 @@
     'rxjs':                       'lib/rxjs',
     'angular2-in-memory-web-api': 'lib/angular2-in-memory-web-api',
     '@angular':                   'lib/@angular',
-    'angular2-modal':             'lib/angular2-modal'
+    'angular2-modal':             'lib/angular2-modal',
+    'angular2-modal/platform-browser': 'lib/angular2-modal/platform-browser',
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: '../boot.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
-    'angular2-modal':             { defaultExtension: 'js'},
-  };
-  var paths = {
-    'angular2-modal':             'lib/angular2-modal'
   };
   var packageNames = [
     '@angular/common',
@@ -29,6 +26,11 @@
     '@angular/router-deprecated',
     '@angular/testing',
     '@angular/upgrade',
+
+    'angular2-modal',
+    'angular2-modal/platform-browser',
+    'angular2-modal/plugins/bootstrap',
+    'angular2-modal/plugins/vex',
   ];
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
   packageNames.forEach(function(pkgName) {
@@ -37,7 +39,7 @@
   var config = {
     map: map,
     packages: packages,
-    paths: paths
+    defaultJSExtensions: true,
   }
 
   // filterSystemConfig - index.html's chance to modify config before we register it.
